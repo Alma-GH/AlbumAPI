e9150ec1fcc8d65cab69427fdf76d43af5e8b51a
Roman Kalimullin <cuni.21@yandex.ru>
2022-02-22 16:09:18 +0300

    fileService

--- FIleService.js
+++ FIleService.js
@@ -1,12 +1,19 @@
 
 import * as path from 'path';
 import fsExtra from "fs-extra"
+import * as fs from "fs"
 
 class FileService {
     saveFile(file, folder) {
         try {
-            const filePath = path.resolve('static/' + folder, file.name);
-            file.mv(filePath);
+
+            fs.mkdir("static", err=>{
+                fs.mkdir("static/" + folder, err=>{
+                    const filePath = path.resolve('static/' + folder, file.name);
+                    file.mv(filePath);
+                })
+            })
+
         } catch (e) {
             console.log(e)
         }
